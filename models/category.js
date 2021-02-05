aad8e14e7e594f56838eb5b47ec969d503cdeca9
NefferKSB <kennanrsb@gmail.com>
2021-02-04 23:46:49 -0500

    Created category, post, and user models

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+let CategorySchema = new mongoose.Schema({
+    id: String,
+    catName: String,
+    catDesc: String,
+    catImgUrl: String,
+    catContent: String,
+    updated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
