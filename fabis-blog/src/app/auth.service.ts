2e4aa38f3b46b07056687a246106f3cfe9471efc
NefferKSB <kennanrsb@gmail.com>
2021-02-06 01:39:21 -0500

    Added env variables for backend url

--- fabis-blog/src/app/auth.service.ts
+++ fabis-blog/src/app/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+
+const BACKEND_URL = environment.apiURL + '/auth/';
 
 @Injectable({
   providedIn: 'root'
