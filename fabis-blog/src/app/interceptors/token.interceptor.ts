7db2e7fb25cfb6be23a358086bf51aea4129f39b
NefferKSB <kennanrsb@gmail.com>
2021-02-05 22:32:15 -0500

    Added HttpInterceptor

---
+++ fabis-blog/src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+  }
+}
