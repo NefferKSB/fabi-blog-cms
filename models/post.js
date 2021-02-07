15cc7d2015526bc858d94b9cbc995e1ba6625865
NefferKSB <kennanrsb@gmail.com>
2021-02-06 21:14:56 -0500

    Updated post model

--- models/post.js
+++ models/post.js
@@ -1,21 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema({
-    category: {
-        type: Schema.Types.ObjectId,
-        ref: 'Category'
-    },
-    id: String,
-    postTitle: String,
-    postAuthor: String,
-    postDesc: String,
-    postContent: String,
-    postReference: String,
-    postImgUrl: String,
-    created: { type: Date },
-    updated: { 
-        type: Date, 
-        default: Date.now 
-    }
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
+
+var PostSchema = new mongoose.Schema({
+  category : { type: Schema.Types.ObjectId, ref: 'Category' },
+  id: String,
+  postTitle: String,
+  postAuthor: String,
+  postDesc: String,
+  postContent: String,
+  postReference: String,
+  postImgUrl: String,
+  created: { type: Date },
+  updated: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Post', PostScema);
+module.exports = mongoose.model('Post', PostSchema);
