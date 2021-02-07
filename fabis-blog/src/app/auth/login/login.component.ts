2b5a30743a152868c1c588e983726664d8c066a6
NefferKSB <kennanrsb@gmail.com>
2021-02-07 11:40:10 -0500

    Added login componenet logic

--- fabis-blog/src/app/auth/login/login.component.ts
+++ fabis-blog/src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  username = '';
+  password = '';
+  matcher = new MyErrorStateMatcher();
+  isLoadingResults = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService) {
+    this.loginForm = formBuilder.group({});
+   }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      'username': [null, Validators.required],
+      'password': [null, Validators.required]
+    });
   }
 
+  //Function for handling form submission
+  onFormSubmit(form: NgForm) {
+    this.authService.login(form)
+      .subscribe(res => {
+        console.log(res);
+        if(res.token) {
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['admin']);
+        }
+      }, err => {
+        console.log(err);
+      });
+  }
+
+  //Navigate to the register page
+  register() {
+    this.router.navigate(['register']);
+  }
+}
+
+//Handles form validations
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
 }
