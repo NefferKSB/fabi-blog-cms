172b4918e9d5773ed735c91cdde80606cffbbf83
NefferKSB <kennanrsb@gmail.com>
2021-02-08 12:11:31 -0500

    Updated the mat-error ngIf

--- fabis-blog/src/app/category/category-add/category-add.component.ts
+++ fabis-blog/src/app/category/category-add/category-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CategoryService } from '../../services/category.service';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category-add',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-add.component.scss']
 })
 export class CategoryAddComponent implements OnInit {
+  categoryForm: FormGroup = new FormGroup({});
+  isLoadingResults = false;
 
-  constructor() { }
+  constructor(private router: Router, private api: CategoryService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.categoryForm = this.formBuilder.group({
+      catName : ['', Validators.required],
+      catDesc : ['', Validators.required],
+      catImgUrl : ['', Validators.required],
+      catContent : ['', Validators.required]
+    });
   }
 
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.addCategory(this.categoryForm.value)
+      .subscribe((res: any) => {
+          const id = res._id;
+          this.isLoadingResults = false;
+          this.router.navigate(['/category-details', id]);
+        }, (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+  }
 }
